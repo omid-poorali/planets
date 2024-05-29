6c0773f6f6c20766f98b95e68707ca3769a3c23d
omid <omidpoorali1992@gmail.com>
2024-05-29 22:09:57 +0330

    feat: react

---
+++ src/lib/react.ts
@@ -0,0 +1,92 @@
+// ---- Library --- //
+export const React = {
+  createElement: (tag, properties, ...children) => {
+    if (typeof tag === 'function') {
+      return tag(properties, ...children);
+    }
+    return {
+      tag,
+      props: properties,
+      children
+    };
+  }
+};
+
+// ---- Library --- //
+const myAppState = [];
+let myAppStateCursor = 0;
+
+export const useState = (initialState) => {
+  // get the cursor for this useState
+  const stateCursor = myAppStateCursor;
+  // Check before setting AppState to initialState (reRender)
+  myAppState[stateCursor] = myAppState[stateCursor] || initialState;
+  console.log(
+    `useState is initialized at cursor ${stateCursor} with value:`,
+    myAppState[stateCursor]
+  );
+  const setState = (newState) => {
+    console.log(
+      `setState is called at cursor ${stateCursor} with newState value:`,
+      newState
+    );
+    myAppState[stateCursor] = newState;
+    // Render the UI fresh given state has changed.
+    reRender();
+  };
+  // prepare the cursor for the next state.
+  myAppStateCursor++;
+  console.log('stateDump', myAppState);
+  return [myAppState[stateCursor], setState];
+};
+
+// ---- Library --- //
+export const renderMethod = (element, container) => {
+  let domElement_;
+  // 0. Check the type of el
+  //    if string we need to handle it like text node.
+  if (typeof element === 'string' || typeof element === 'number') {
+    // create an actual Text Node
+    domElement_ = document.createTextNode(String(element));
+    container.append(domElement_);
+    // No children for text node so we return.
+    return;
+  }
+  // 1. First create the document node corresponding el
+  domElement_ = document.createElement(element.tag);
+  // 2. Set the props on domEl
+  let elementProperties = element.props
+    ? Object.keys(element.props)
+    : undefined;
+  if (elementProperties && elementProperties.length > 0) {
+    for (const property of elementProperties)
+      domElement_[property] = element.props[property];
+  }
+  // 3. Handle creating the Children.
+  if (element.children && element.children.length > 0) {
+    // When child is rendered, the container will be
+    // the domEl we created here.
+    for (const node of element.children) renderMethod(node, domElement_);
+  }
+  // 4. append the DOM node to the container.
+  container.append(domElement_);
+};
+
+let domElement;
+let rootElement;
+
+export const render = (element, container) => {
+  rootElement = element;
+  domElement = container;
+  renderMethod(React.createElement(rootElement, {}), domElement);
+};
+
+const reRender = () => {
+  console.log('reRender-ing :)');
+  // reset/clean whatever is rendered already
+  domElement.innerHTML = '';
+  // Reset the global state cursor
+  myAppStateCursor = 0;
+  // then render Fresh
+  renderMethod(React.createElement(rootElement, {}), domElement);
+};
--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,63 +1,23 @@
-import '../styles/global.scss';
-import '../styles/global.css';
+import { useState, React, render } from "../lib/react";
 
-// This will our main app file.
+// ---- Application --- //
+const App = () => {
+  const [name, setName] = useState('Omid');
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>Hello {name}!</h2>
+      <p>I am a pargraph</p>
+      <input
+        type="text"
+        value={name}
+        onchange={(e) => setName(e.target.value)}
+      />
+      <h2> Counter value: {count}</h2>
+      <button onclick={() => setCount(count + 1)}>+1</button>
+      <button onclick={() => setCount(count - 1)}>-1</button>
+    </div>
+  );
+};
 
-// ---- Library --- //
-const React = {
-    createElement: (tag, props, ...children) => {
-      if (typeof tag === 'function') {
-        return tag(props, ...children);
-      }
-      const el = {
-        tag,
-        props,
-        children,
-      };
-      return el;
-    },
-  };
-  
-  // ---- Library --- //
-  const render = (el, container) => {
-    let domEl;
-    // 0. Check the type of el
-    //    if string we need to handle it like text node.
-    if (typeof el === 'string') {
-      // create an actual Text Node
-      domEl = document.createTextNode(el);
-      container.appendChild(domEl);
-      // No children for text node so we return.
-      return;
-    }
-    // 1. First create the document node corresponding el
-    domEl = document.createElement(el.tag);
-    // 2. Set the props on domEl
-    let elProps = el.props ? Object.keys(el.props) : null;
-    if (elProps && elProps.length > 0) {
-      elProps.forEach((prop) => (domEl[prop] = el.props[prop]));
-    }
-    // 3. Handle creating the Children.
-    if (el.children && el.children.length > 0) {
-      // When child is rendered, the container will be
-      // the domEl we created here.
-      el.children.forEach((node) => render(node, domEl));
-    }
-    // 4. append the DOM node to the container.
-    container.appendChild(domEl);
-  };
-  
-  // ---- Application --- //
-  const App = () => {
-    const myName = 'omid';
-    return (
-      <div draggable>
-        <h2>Hello {myName}!</h2>
-        <p>I am a pargraph</p>
-        <input type="text" />
-      </div>
-    );
-  };
-  
-  render(<App />, document.getElementById('root'));
-  
+render(App, document.getElementById('root'));
